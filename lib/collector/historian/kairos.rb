ba24809772f93777842f57f69ee6fa991533b187
Ubuntu <ubuntu@jmeter.(none)>
2014-06-24 08:17:41 +0000

    added check

--- lib/collector/historian/kairos.rb
+++ lib/collector/historian/kairos.rb
@@ -21,33 +21,17 @@ module Collector
         data[:name] = "#{@deployment_name}/#{data[:tags][:name].gsub('.', '_')}"
         data[:tags][:name] = data[:key]
         data.delete :key
-=begin
-        File.open('logs/data.logs', 'a') do |file| 
-          file.write(data.to_s + "\n") 
-        end
-        File.open('logs/name.logs', 'a') do |file| 
-          file.write(data[:tags][:name] + "\n")
-        end
-=end
         data
       end
 
       def send_metrics(data)
-        #TODO remove this condition in production
-        #return unless data[:name] == "kairosdb-benchmark" || data[:name] == "DEA"
-        #return unless data[:name] == "datacollector" || data[:name] == "arcadeTrigSim"
+        return if data[:value] == "default"
         Config.logger.debug("Sending metrics to kairos: [#{data.inspect}]")
         body = Yajl::Encoder.encode(data)
         response = @http_client.post(@api_host, body: body, headers: {"Content-type" => "application/json"})
         if response.success?
           Config.logger.info("collector.emit-kairos.success", number_of_metrics: 1, lag_in_seconds: 0)
         else
-=begin
-          File.open('logs/failed.logs', 'a') do |file| 
-            file.write(response.to_s + "\n")
-            file.write(data.to_s + "\n") 
-          end
-=end
           Config.logger.warn("collector.emit-kairos.fail", number_of_metrics: 1, lag_in_seconds: 0)
         end
       end
